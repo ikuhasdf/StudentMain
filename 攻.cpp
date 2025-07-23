ac08d81fee289c1012c895d4276104c621c74edb
ikuhasdf <ikuhasdf@outlook.com>
2025-07-23 15:19:31 +0800

    Update 攻.cpp

--- 攻.cpp
+++ 攻.cpp
@@ -1,21 +1,35 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-	printf("警告！本软件需要管理员权限!\n");
+	printf("警告！本软件需要管理员权限!\n");  // 免责说明
 	printf("警告！运行软件时关闭杀毒软件!\n");
 	printf("输入一:");
 	int a;
+	int b;
 	while (true)
 	{
 		scanf("%d", &a);
 		if (a == 1)
 		{
-			system("taskkill /pid StudentMain.exe /f");
+			system("taskkill /pid StudentMain.exe /f");  // 关闭进程
 			printf("如果系统说没'StudentMain.exe'进程，就不用这款软件，退出即可！\n");
 			printf("攻破成功,打开软件目录'geek.exe'删除进程\n");
-			printf("自动打开...");
-			system("geek.exe");
+			printf("GitHub版没有geek.exe请自行下载，官网地址：https://geekuninstaller.com\n");
+			printf("你要打开吗？输入1打开或输入2关闭：");
+			scanf("%d",&b);
+			if (b == 1)
+			{
+				printf("正在打开...\n");
+				system("start https://geekuninstaller.com");
+				printf("如果没有打开，说明你的电脑没有浏览器！\n");
+
+			}
+			else
+			{
+				printf("正在退出...\n");
+				break;
+			}
 			break;
 			
 		}
@@ -25,6 +39,6 @@ int main()
 			break;
 		}
 	}
-	return 0;
 	system("pause");
+	return 0;
 }
